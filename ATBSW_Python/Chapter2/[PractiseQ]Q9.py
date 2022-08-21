a7e4a9403123207b19b8abe27380cd1564c2c770
JESollo <jann.ed.sollo@gmail.com>
2022-08-21 15:50:59 -0400

    Practise Question 9
    Practise Question 9: Write code that prints Hello if 1 is stored in spam, prints Howdy if 2 is stored in spam, and prints Greetings! if anything else is stored in spam.

---
+++ ATBSW_Python/Chapter2/[PractiseQ]Q9.py
@@ -0,0 +1,12 @@
+# Write your code here :-)
+
+spam = 1
+while spam == 1:
+    print('Hello')
+    spam = spam + 1
+    if spam == 2:
+        print('Howdy')
+    elif spam != 1 or 2:
+        print('Greetings!')
+
+
